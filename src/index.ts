ef0a7816f95b86a66f730d25bb0d4ed93a93291c
Darío Guillén <guillendario@gmail.com>
2024-09-20 17:03:43 -0600

    feat: init console app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hola Mundo')
+import fs from "fs";
+
+const base = 5;
+const path = 'outputs'
+let message = `
+================================================
+            Tabla del ${base}
+================================================\n
+`;
+for (let i = 1; i < 10; i++) {
+  message += `${base} * ${i} = ${base * i}\n`;
+}
+
+console.log(message);
+
+fs.mkdirSync(path, {recursive: true})
+fs.writeFileSync(`${path}/tabla-${base}.txt`, message)
