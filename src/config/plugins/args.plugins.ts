f0e26a843f167356ae167fb98c10e19f46cc7ab1
Darío Guillén <guillendario@gmail.com>
2024-09-25 13:33:34 -0600

    feat: yargs configurations

---
+++ src/config/plugins/args.plugins.ts
@@ -0,0 +1,27 @@
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+export const yarg = yargs(hideBin(process.argv))
+  .option("b", {
+    alias: "base",
+    type: "number",
+    demandOption: true,
+    description: "Multiplication base",
+  })
+  .option("l", {
+    alias: "limit",
+    type: "number",
+    default: 10,
+    description: "Multiplication limit",
+  })
+  .option("s", {
+    alias: "show",
+    type: "boolean",
+    default: false,
+    description: "Show multiplication table",
+  })
+  .check((argv) => {
+    if (argv.b < 1) throw new Error("base should be > 0 ");
+    return true;
+  })
+  .parseAsync();
